d02c14e8abaf7c0eec17de6fc37851bf38cb0b65
AmirSahrani <amirsahrani1@gmail.com>
2023-10-30 21:21:17 +0100

    all

---
+++ dynamic.js
@@ -0,0 +1,9 @@
+var fs = require('fs')
+
+function return_articles(params) {
+  articles =  fs.readdirSync("articles")
+  for (let i = 0; i < articles.len ; i++) {
+    return() 
+  }
+   
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./input.css","./index.html", "./blog.html", "./pictures.html"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'mono': ['Courier']
+    }
+  },
+  plugins: [],
+}
+
